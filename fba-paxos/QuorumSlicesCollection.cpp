1e8dfdd605ecd26b483da113434d2854b2790828
PLR <51248199+plredmond@users.noreply.github.com>
2025-04-03 10:47:01 -0700

    fix missing const& annotations; fix m[node] to m.find(node)->second because m is const&

--- fba-paxos/QuorumSlicesCollection.cpp
+++ fba-paxos/QuorumSlicesCollection.cpp
@@ -7,6 +7,10 @@
 
 
 // HACK: Ought to be some way to sort NodeID, but this is probably not it.
+//
+// NOTE: the std::set and std::map types in this file all have <...,
+// decltype(nodeid_cmp)*> because stellar::NodeID doesn't have operator<
+// defined. It's pretty messy.
 bool nodeid_cmp(stellar::NodeID const& a, stellar::NodeID const& b)
 {
     return a.ed25519() < b.ed25519();
@@ -16,7 +20,7 @@ bool nodeid_cmp(stellar::NodeID const& a, stellar::NodeID const& b)
 // (interface)
 
 template<class NID>
-using Slice = std::set<NID, decltype(nodeid_cmp)>;
+using Slice = std::set<NID, decltype(nodeid_cmp)*>;
 
 template<class NID, class QuorumSlices> class QuorumChecker
 {
@@ -25,11 +29,11 @@ template<class NID, class QuorumSlices> class QuorumChecker
 
         // Search for a quorum among the given a map (of nodes and their
         // last-known quorum slices), otherwise return the empty set.
-        virtual Slice<NID> findQuorum(std::map<NID, QuorumSlices>) = 0;
+        virtual Slice<NID> findQuorum(std::map<NID, QuorumSlices, decltype(nodeid_cmp)*> const&) = 0;
 
         // Is the candidate slice a quorum for the given quorum-slices? (I.e.
         // Is any one of the quorum-slices a subset of the candidate slice?)
-        virtual bool isQuorumSlice(Slice<NID>, QuorumSlices) = 0;
+        virtual bool isQuorumSlice(Slice<NID> const&, QuorumSlices const&) = 0;
 
         // TODO: is-blocking-set function?
 };
@@ -49,7 +53,7 @@ class NaiveQuorumChecker : public QuorumChecker< NID, NaiveQuorumSlices<NID> >
 
     public:
 
-        virtual X findQuorum(std::map<NID, XS> const& m)
+        virtual X findQuorum(std::map<NID, XS, decltype(nodeid_cmp)*> const& m)
         {
             X candidate(nodeid_cmp);
             for (auto const& kv : m)
@@ -63,7 +67,7 @@ class NaiveQuorumChecker : public QuorumChecker< NID, NaiveQuorumSlices<NID> >
                 size_before_filtering = candidate.size();
                 for (auto const& node : candidate) // std::erase_if in c++20
                 {
-                    if (!isQuorumSlice(candidate, m[node]))
+                    if (!isQuorumSlice(candidate, m.find(node)->second))
                     {
                         candidate.erase(node); // XXX possible iterator invalidation
                     }
@@ -79,7 +83,6 @@ class NaiveQuorumChecker : public QuorumChecker< NID, NaiveQuorumSlices<NID> >
             {
                 // a⊆b ≅ (∅ ≡ a⁄b)
                 X extras(nodeid_cmp);
-                //X extras; // gives cmp error
                 std::set_difference(slice.begin(), slice.end(),
                                     candidate.begin(), candidate.end(),
                                     std::inserter(extras, extras.begin()),
