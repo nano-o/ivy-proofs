df20dfb80904cd696a8a4c958f268c3510cedf09
PLR <51248199+plredmond@users.noreply.github.com>
2025-04-02 23:07:54 -0700

    rename type parameter NodeId to NID to avoid confusion with stellar::NodeID

--- fba-paxos/QuorumSlicesCollection.cpp
+++ fba-paxos/QuorumSlicesCollection.cpp
@@ -8,21 +8,21 @@
 
 // (interface)
 
-template<class NodeId>
-using Slice = std::set<NodeId>;
+template<class NID>
+using Slice = std::set<NID>;
 
-template<class NodeId, class QuorumSlices> class QuorumChecker
+template<class NID, class QuorumSlices> class QuorumChecker
 {
     public:
         virtual ~QuorumChecker() {}
 
         // Search for a quorum among the given a map (of nodes and their
         // last-known quorum slices), otherwise return the empty set.
-        virtual Slice<NodeId> findQuorum(std::map<NodeId, QuorumSlices>) = 0;
+        virtual Slice<NID> findQuorum(std::map<NID, QuorumSlices>) = 0;
 
         // Is the candidate slice a quorum for the given quorum-slices? (I.e.
         // Is any one of the quorum-slices a subset of the candidate slice?)
-        virtual bool isQuorumSlice(Slice<NodeId>, QuorumSlices) = 0;
+        virtual bool isQuorumSlice(Slice<NID>, QuorumSlices) = 0;
 
         // TODO: is-blocking-set function?
 };
@@ -31,18 +31,18 @@ template<class NodeId, class QuorumSlices> class QuorumChecker
 // (a naive implementation)
 
 // Quorum slices, stored naively.
-template<class NodeId>
-using NaiveQuorumSlices = std::vector< Slice<NodeId> >;
+template<class NID>
+using NaiveQuorumSlices = std::vector< Slice<NID> >;
 
-template<class NodeId>
-class NaiveQuorumChecker : public QuorumChecker< NodeId, NaiveQuorumSlices<NodeId> >
+template<class NID>
+class NaiveQuorumChecker : public QuorumChecker< NID, NaiveQuorumSlices<NID> >
 {
-    using X = Slice<NodeId>;
-    using XS = NaiveQuorumSlices<NodeId>;
+    using X = Slice<NID>;
+    using XS = NaiveQuorumSlices<NID>;
 
     public:
 
-        virtual Slice<NodeId> findQuorum(std::map<NodeId, XS> m)
+        virtual Slice<NID> findQuorum(std::map<NID, XS> m)
         {
             X x;
             // TODO
@@ -58,13 +58,13 @@ class NaiveQuorumChecker : public QuorumChecker< NodeId, NaiveQuorumSlices<NodeI
 };
 
 
-template<class NodeId, class QuorumSlices>
+template<class NID, class QuorumSlices>
 void local_node_example(
-        QuorumChecker<NodeId, QuorumSlices> const& checker,
-        std::map<NodeId, QuorumSlices> const& nodeStates,
+        QuorumChecker<NID, QuorumSlices> const& checker,
+        std::map<NID, QuorumSlices> const& nodeStates,
         QuorumSlices const& localNodeSlices)
 {
-    Slice<NodeId> quorum = checker.findQuorum(nodeStates);
+    Slice<NID> quorum = checker.findQuorum(nodeStates);
     if (quorum.empty())
     {
         std::cout << "No quorum set in the given node states." << std::endl;
