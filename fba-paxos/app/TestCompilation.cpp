f5d3f9aa9f37555b9bc8dc445f88597a629c5f8b
PLR <51248199+plredmond@users.noreply.github.com>
2025-04-08 16:48:31 -0700

    port tests from https://github.com/nano-o/python-fbas/blob/83c7c7c7b5c8e4fecbefa88cbafac6ad78ba5e33/tests/fbas_graph_test.py#L24-L36

--- fba-paxos/app/TestCompilation.cpp
+++ fba-paxos/app/TestCompilation.cpp
@@ -6,6 +6,53 @@
 #include <libscp/QSetQuorumChecker.hpp>
 #include <libscp/NaiveQuorumChecker.hpp>
 
+using NodeQSets = std::map< stellar::NodeID, stellar::SCPQuorumSet, decltype(nodeid_cmp)* >;
+
+// constructor that ensures we add the nodeid_cmp (else segfaults occur on insert)
+NodeQSets node_qsets_empty()
+{
+    NodeQSets m(nodeid_cmp);
+    return m;
+}
+
+// restrict test data to specified keys
+NodeQSets test_restrict(NodeQSets const& m1, std::vector<std::string> const& ks)
+{
+    auto m2 = node_qsets_empty();
+    for (auto const& k: ks)
+    {
+        auto it = m1.find(conv_nid(k));
+        assert(it != m1.end()); // test_restrict should fail if a name isn't in the test data
+        m2.insert(*it);
+        std::cout << k << ' ';
+    }
+    std::cout << ';' << std::endl;
+    return m2;
+}
+
+void test_QSetQuorumChecker()
+{
+    auto test_data = node_qsets_empty();
+    for (std::string const& name : {"PK11", "PK12", "PK13", "PK21", "PK22", "PK23", "PKX"})
+    {
+        stellar::NodeID k = conv_nid(name);
+        stellar::SCPQuorumSet v;
+        assert(0 < load_xdr("data/" + name + ".xdr", v)); // test should fail if an expected test file isn't found
+        test_data.insert({k, v});
+    }
+    // XXX tests ported from https://github.com/nano-o/python-fbas/blob/83c7c7c7b5c8e4fecbefa88cbafac6ad78ba5e33/tests/fbas_graph_test.py#L24-L36
+    QSetQuorumChecker qsqc;
+    assert( isQuorum(qsqc, test_restrict(test_data, {"PK11", "PK12", "PK13"})));
+    assert( isQuorum(qsqc, test_restrict(test_data, {"PK11", "PK12"})));
+    assert(!isQuorum(qsqc, test_restrict(test_data, {"PK11"})));
+    assert(!isQuorum(qsqc, test_restrict(test_data, {"PK11", "PK12", "PK13", "PK21"})));
+    assert(!isQuorum(qsqc, test_restrict(test_data, {"PK13", "PK21"})));
+    assert(!isQuorum(qsqc, test_restrict(test_data, {"PK11", "PK12", "PK13", "PK21", "PK22", "PK23"})));
+    //fbas.is_quorum({"PK11", "PK12", "PK13", "NON_EXISTENT"}) // test not ported
+    assert(!isQuorum(qsqc, test_restrict(test_data, {"PK11", "PK12", "PK13", "PKX"})));
+    assert( isQuorum(qsqc, test_restrict(test_data, {"PK11", "PK12", "PKX", "PK22", "PK23"})));
+    assert(!isQuorum(qsqc, test_restrict(test_data, {"PK11", "PK12", "PKX", "PK22"})));
+}
 
 // test that we can do some things with the QC interface
 template<class NID, class QuorumSlices>
