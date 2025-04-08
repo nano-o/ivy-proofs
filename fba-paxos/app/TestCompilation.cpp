a77c7a3ad15f77f5cce15eaa09cc8a980e531dcd
PLR <51248199+plredmond@users.noreply.github.com>
2025-04-08 16:45:54 -0700

    reduce size of test main

--- fba-paxos/app/TestCompilation.cpp
+++ fba-paxos/app/TestCompilation.cpp
@@ -28,40 +28,13 @@ int local_node_example(
 }
 
 int main() {
-    stellar::SCPQuorumSet quorumSet;
-    quorumSet.threshold = 2;
+    test_QSetQuorumChecker();
 
-    stellar::NodeID validator1(stellar::PUBLIC_KEY_TYPE_ED25519);
-    xdr::opaque_array<32> key1 = {};
-    key1[0] = 1;
-    validator1.type(stellar::PUBLIC_KEY_TYPE_ED25519);
-    validator1.ed25519() = key1;
-    quorumSet.validators.push_back(validator1);
-
-    stellar::SCPQuorumSet innerSet;
-    innerSet.threshold = 1;
-    stellar::NodeID validator2(stellar::PUBLIC_KEY_TYPE_ED25519);
-    xdr::opaque_array<32> key2 = {};
-    key2[0] = 42;
-    validator2.type(stellar::PUBLIC_KEY_TYPE_ED25519);
-    validator2.ed25519() = key2;
-    innerSet.validators.push_back(validator2);
-
-    quorumSet.innerSets.push_back(innerSet);
-
-    // Print the SCPQuorumSet
-    std::cout << "SCPQuorumSet:" << std::endl;
-    std::cout << "Threshold: " << quorumSet.threshold << std::endl;
-    std::cout << "Validators: " << quorumSet.validators.size() << std::endl;
-    std::cout << "InnerSets: " << quorumSet.innerSets.size() << std::endl;
-
-    for (const auto& inner : quorumSet.innerSets) {
-        std::cout << "  InnerSet Threshold: " << inner.threshold << std::endl;
-        std::cout << "  InnerSet Validators: " << inner.validators.size() << std::endl;
+    {
+        // XXX test that these templates can be instantiated
+        NaiveQuorumChecker<stellar::NodeID> nqc;
+        assert(-1 == local_node_example(nqc,
+                std::map<stellar::NodeID, std::vector<Slice<stellar::NodeID> >, decltype(nodeid_cmp)*>(),
+                std::vector<Slice<stellar::NodeID> >()));
     }
-
-    NaiveQuorumChecker<stellar::NodeID> checker;
-    QSetQuorumChecker checkerB;
-
-    return 0;
 }
